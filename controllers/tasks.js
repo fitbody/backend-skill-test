a6f49249d8d85d846d07ca8e0c65a0e430a20345
Daniel Goycochea <danielgoycocheardz2gmail.com>
2021-05-14 21:52:23 -0500

    update controllers

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,35 +1,62 @@
 const { response } = require("express");
+const Task = require("../models/tasks");
+const Tasks = require("../models/tasks");
 
 const tasksGet = (req, res, next) => {
-  res.json({
-    message: "todo esta chido en el get",
-  });
+  Tasks.findAll({})
+    .then((result) => res.json(result))
+    .catch((err) => {
+      res.status(412).json({
+        message: err.message,
+      });
+    });
+};
+
+const taskGet = (req, res, next) => {
+  Tasks.findOne({ where: {id: req.params.id} })
+    .then((result) => res.json(result))
+    .catch((err) => {
+      res.status(412).json({
+        message: err.message,
+      });
+    });
 };
 const tasksPost = (req, res, next) => {
-  res.json({
-    message: "todo esta chido en el post",
-  });
-};
-const tasksPut = (req, res, next) => {
-  res.json({
-    message: "todo esta chido en el put",
-  });
+  Task.create(req.body)
+    .then((result) => res.json({ message: "successfull", result }))
+    .catch((err) => {
+      res.status(412).json({
+        message: err.message,
+      });
+    });
 };
 const tasksDelete = (req, res, next) => {
-  res.json({
-    message: "todo esta chido en el delete",
-  });
+  Task.destroy({ where: { id: req.params.id } })
+    .then(() => {
+      res.json({ message: `Task id ${req.params.id} delete` });
+    })
+    .catch((err) => {
+      res.status(412).json({
+        message: err.message,
+      });
+    });
 };
 const tasksPatch = (req, res, next) => {
-  res.json({
-    message: "todo esta chido en el patch",
-  });
+  Task.update(req.body, { where: {id: req.params.id} })
+    .then(() => {
+      res.json(`successful`);
+    })
+    .catch((err) => {
+      res.status(412).json({
+        message: err.message,
+      });
+    });
 };
 
 module.exports = {
   tasksGet,
+  taskGet,
   tasksPost,
-  tasksPut,
   tasksDelete,
-  tasksPatch
+  tasksPatch,
 };
